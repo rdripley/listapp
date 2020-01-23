360d3aa639740276ecf7bf5308244a235b34f676
rdripley <russellripley@hotmail.com>
2020-01-22 21:23:21 -0700

    add app to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function List() {
+  /* this will need to have the value of input passed to it to the list */
+  /* look up how to not show the list if the list is empty */
+  return (
+    <ol>
+      <li>{/* need to look up how to loop through an array*/}</li>
+    </ol>
+  );
+}
+
+function Input() {
+  /* this will need a onClick handler for the submit button */
+  return (
+    <div>
+      <input type='text' placeholder='Enter Name here'></input>
+      <input type='submit'></input>
+    </div>
+  );
+}
 
 function App() {
+  /* App will need to have the onClick function defined here so I can pass
+  it down to the List component */
+  /* will need the following 
+
+  history: [ 
+    {
+    lists: Array().fill(null)
+    }
+  ]
+  listNumber: 0
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.listNumber + 1);
+    const current = history[history.length - 1];
+    const list = current.lists.slice();
+    this.setState({
+      history: history.concat([{lists: lists}]),
+      listNumber: history.length
+    });
+  }
+
+  undoDeletion(list) {
+    this.setState({
+      listNumber: list
+    })
+
+  const listItems = history.map((listItem, listLength)) => {
+    const desc = listItem ? ""
+  }
+  } 
+  */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Input />
+      <List />
     </div>
   );
 }
